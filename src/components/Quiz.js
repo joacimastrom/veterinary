0c39d06f76c3567eb31324132819008421765931
Joacim Åström <joacim.astrom@gmail.com>
2023-09-07 14:13:41 +0200

    Fix scroll and add jumpTo function

--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -9,12 +9,13 @@ import {
   FormControlLabel,
   FormGroup,
   Grow,
+  Input,
   Slider,
   Switch,
   TextField,
   Typography,
 } from "@mui/material";
-import { useState } from "react";
+import { useRef, useState } from "react";
 import {
   checkEqual,
   checkPartOfWord,
@@ -54,6 +55,8 @@ export const Quiz = ({ domain, onBack, shuffle }) => {
   const [mode, setMode] = useState({ from: "sv", to: "la" });
   const [showSettings, setShowSettings] = useState(false);
   const [hintLength, setHintLength] = useState(1);
+  const [jumpTo, setJumpTo] = useState("");
+  const inputRef = useRef();
 
   const { group, subGroup, ...currentWord } = wordSet[currentIndex];
 
@@ -61,14 +64,18 @@ export const Quiz = ({ domain, onBack, shuffle }) => {
   const partlyCorrect = checkPartOfWord(textValue, toWord);
   const correctAnswer = checkEqual(textValue, toWord);
 
-  const changeQuestion = (direction) => {
+  const changeQuestion = (nextIndex) => {
     setShowAlternatives(false);
     setMode(getMode(modeSlider));
     setTextValue("");
-    setAlternatives(getAlternatives(currentIndex + direction, wordSet));
-    setCurrentIndex((current) => current + direction);
+    setJumpTo("");
+    setShowSettings(false);
+    setAlternatives(getAlternatives(nextIndex, wordSet));
+    setCurrentIndex(nextIndex);
     setHintLength(1);
+    inputRef.current.focus();
   };
+
   const onLastQuestion = currentIndex === wordSet.length - 1;
 
   const getNextLetter = () => {
@@ -84,6 +91,8 @@ export const Quiz = ({ domain, onBack, shuffle }) => {
       setHintLength((curr) => getHintLength(curr, toWord));
     }
   };
+
+  const validJumpTo = jumpTo > 0 && jumpTo <= wordSet.length;
 
   return (
     <>
@@ -132,8 +141,11 @@ export const Quiz = ({ domain, onBack, shuffle }) => {
           )}
           <TextField
             onKeyDown={(e) =>
-              e.key === "Enter" && correctAnswer && changeQuestion(1)
+              e.key === "Enter" &&
+              correctAnswer &&
+              changeQuestion(currentIndex + 1)
             }
+            inputRef={inputRef}
             value={textValue}
             onChange={onChange}
             id="standard-basic"
@@ -199,7 +211,7 @@ export const Quiz = ({ domain, onBack, shuffle }) => {
           <Button
             size="small"
             disabled={!currentIndex}
-            onClick={() => changeQuestion(-1)}
+            onClick={() => changeQuestion(currentIndex - 1)}
           >
             Förgående
           </Button>
@@ -207,7 +219,9 @@ export const Quiz = ({ domain, onBack, shuffle }) => {
             size="small"
             variant="contained"
             disabled={!correctAnswer}
-            onClick={onLastQuestion ? onBack : () => changeQuestion(1)}
+            onClick={
+              onLastQuestion ? onBack : () => changeQuestion(currentIndex + 1)
+            }
             color="success"
           >
             {onLastQuestion ? "Klar" : "Nästa"}
@@ -255,6 +269,27 @@ export const Quiz = ({ domain, onBack, shuffle }) => {
                 label="Visa undergrupp"
               />
             </FormGroup>
+            <Typography mt={1} mb={-1}>
+              Hoppa till ord
+            </Typography>
+            <Box>
+              <Input
+                type="number"
+                value={jumpTo}
+                onKeyDown={(e) =>
+                  e.key === "Enter" && validJumpTo && changeQuestion(jumpTo - 1)
+                }
+                onChange={(e) => setJumpTo(e.target.value)}
+                min={1}
+                max={wordSet.length}
+              />
+              <Button
+                disabled={!validJumpTo}
+                onClick={() => changeQuestion(jumpTo - 1)}
+              >
+                Hoppa
+              </Button>
+            </Box>
           </Collapse>
         </CardContent>
       </Card>
